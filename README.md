d22a0bc34f6e7b62afc7b11f1b092d21b9b6146c
carlosdre2018 <carlos.chavez.b@uni.pe>
2023-02-12 23:58:35 -0500

    Juego terminado, interfaz simple, readme.md modificado, se añade tambien un .gitignore

--- README.md
+++ README.md
@@ -1,18 +1,28 @@
-# Project
+# Juego del ahorcado
 
-Hello new developers; Now is the time to start working on your project.
-Hangman game! is the name of the project; If you want to know how to play this game, check the link below.
+Hola jugadores, les damos la bienvenida al juego del ahorcado.
+Se presenta una interfaz y un codigo simples para poder jugar este clasico juego considerando varios escenarios.
 
-https://www.youtube.com/watch?v=leW9ZotUVYo
 
-## Instructions
-- Read all the bank of words or phrases from the JSON file. Select one and start the game.
-- You will always receive numbers and letters, not symbols(except blank characters)
-- Use everything you have learned so far, and try new things. Not include classes.
-- Try to create the best user experience in your terminal.
+## Consideracioness
+- Se puede cometer hasta 6 errores.
+- No se descartan puntos si una letra correcta se vuelve a digitar.
+- Tampoco se descartaran si se digita más de una letra o número.
+- Las unicas entradas validas seran letras o números, no simbolos ni conjunto de letras o números o ambos.
+- Para terminar el juego será necesario que se digite N en la pregunta, caso contrario se volverá a jugar.
+- La palabra u oración a elegir se debera de sacar mediante el archivo .json [data]
+- El orden de las partes del ahorcado son: cabeza, cuello, brazo izquierdo, brazo derecho, pierna derecha, pierna izquierda
+- El programa reconoce tildes y mayusculas por lo que no lo toma como distinto, es decir que una letra mayuscula será considerada tambien como minuscula.
+- El punto anterior tambien incluye las tildes.
 
 ## Bonus
 - write tests.
 
 ### What was your creative process:
-Write here all you want.
+Se usaron variables de cadena, enteras y booleanas asi como dos funciones para poder resolver este problema.
+- La variable desplaz se usa para verificar si una letra se encuentra en la oracion o palabra a adivinar.
+- Las variables booleanas se usaron para identificar la entrada o no a condicionales.
+- En la lista corrects se añaden todas las letras que ya se adivinaron correctamente.
+- La variable verif sirve para identificar cuando se ha ganado el juego o no.
+- No se han usado librerias externas a las ya incluidas en python por defecto  como json o random, por lo tanto no se requiere un requirement.txt
+ 
--- main.py
+++ main.py
@@ -1,1 +1,132 @@
-# code here
+import json
+import random
+
+def tildes(vocal):
+    if vocal == "á":
+        vocal = "a"
+    elif vocal == "é":
+        vocal = "e"
+    elif vocal == "í":
+        vocal = "i"
+    elif vocal == "ó":
+        vocal = "o"
+    elif vocal == "ú":
+        vocal = "u"
+    return vocal
+def tildes_inv(vocal):
+    if vocal == "a":
+        vocal = "á"
+    elif vocal == "e":
+        vocal = "é"
+    elif vocal == "i":
+        vocal = "í"
+    elif vocal == "o":
+        vocal = "ó"
+    elif vocal == "u":
+        vocal = "ú"
+    return vocal
+
+with open("words.json") as my_words:
+    words = json.loads(my_words.read())
+play = True
+
+print("\n\n---------------------Bienvenido al juego del ahorcado ------------------------")
+while play:
+    r = random.randint(0, len(words['data']) - 1)
+    word_game = words['data'][r]
+    contador = 0
+    corrects = []
+    print("\n\nSe ha seleccionado aleatoriamente una palabra a adivinar: \n")
+
+    for a in word_game:
+        if a != " ":
+            print("_", end=" ")
+        elif a == " ":
+            print("   ", end=" ")
+    while contador < 6:
+        verif = 0
+        desplaz = 0
+        max_dig = True
+        repetido = True
+        print("\n\n\n*******************************************************************")
+        n = input("\n\nDigite su letra o numero: ")
+        if len(n)>1:
+            print("\n\n-----------Solo una letra o número a la vez-----------")
+            max_dig = False
+        elif len(n)==1:
+            if (n in corrects) or (n.swapcase() in corrects) or (tildes(n) in corrects) or (tildes(n).swapcase() in corrects) or (tildes_inv(n) in corrects) or (tildes_inv(n).swapcase() in corrects):
+                print("\n\n-----------El valor dado ya fue digitado-----------\n\n")
+                repetido = False
+        for i in word_game:
+            if (i == n or i == n.swapcase() or n == tildes(i) or n == tildes(i).swapcase() or i == tildes(n) or i == tildes(n).swapcase()) and repetido:
+                print(f"\nCORRECTO, la letra <<{i}>> esta en la frase\n")
+                corrects.append(i)
+                break
+            elif i != n:
+                desplaz = desplaz + 1
+            if desplaz == len(word_game) and max_dig and repetido:
+                print(f"\nINCORRECTO, la letra <<{n}>> no se encuentra en la frase")
+                contador = contador + 1
+        if contador == 1:
+            print("\n\t _________")
+            print("\t |")
+            print("\t🙂\n\n")
+        if contador == 2:
+            print("\n\t _________")
+            print("\t |")
+            print("\t🙂")
+            print("\t |\n\n")
+        if contador == 3:
+            print("\n\t _________")
+            print("\t |")
+            print("\t🙂")
+            print("\t |")
+            print("\t  \ \n\n")
+        if contador == 4:
+            print("\n\t _________")
+            print("\t |")
+            print("\t🙂")
+            print("\t |")
+            print("\t/|\ \n\n")
+        if contador == 5:
+            print("\n\t _________")
+            print("\t |")
+            print("\t🙂")
+            print("\t |")
+            print("\t/|\ ")
+            print("\t/  \n\n")
+        for j in word_game:
+            desplaz2 = 0
+            if (j in corrects) or (j.swapcase() in corrects):
+                print(f"{j}", end =" ")
+            else:
+                if j!= " ":
+                    print("_", end = " ")
+                    verif = verif + 1
+                elif j == " ":
+                    print("   ", end= " ")
+        if len(corrects) == 0:
+            for a in word_game:
+                if a != " ":
+                    print("_", end=" ")
+                elif a == " ":
+                    print("   ", end=" ")
+        if len(corrects) > 0:
+            if verif == 0:
+                print("\n\n\nHaz ganado el juego, FELICIDADES")
+                game = input("Desea seguir jugando? (N para dejar de jugar): ")
+                break
+    if contador == 6:
+        print("\n\n\t _________")
+        print("\t |")
+        print("\t🙂")
+        print("\t |")
+        print("\t/|\ ")
+        print("\t/ \ ")
+        print("\n\nHas perdido, suerte a la proxima\n\n")
+
+        print(f"\tLa frase a adivinar era: {word_game}")
+        game = input("\n\nDesea seguir jugando? (N para dejar de jugar) : ")
+        if game == "N":
+            play = False
+print("\n\nGracias por jugar al ahorcado, buen dia.")
